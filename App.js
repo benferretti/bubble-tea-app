58baed8be267b8c582fb23773436c220ff0493d5
buetasg <gabriel.buetas@epitech.eu>
2021-04-03 20:07:38 +0200

    Flatlist working

--- App.js
+++ App.js
@@ -1,89 +1,205 @@
+import { StatusBar } from 'expo-status-bar';
+import React,{Children, useState} from 'react';
+import { 
+  StyleSheet, Text, View,
+  SafeAreaView, FlatList, Modal,
+  TouchableOpacity, TextInput 
+} from 'react-native';
 
-import * as React from 'react';
-import { List, Button, ProgressBar, Text, Chip, Colors, TextInput} from 'react-native-paper';
-import {SafeAreaView, StyleSheet, View, ScrollView} from 'react-native';
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import OrderScreen from './screens/Order'
-import ConfirmationScreen from './screens/Confirmation'
-import bubbletea from './data';
-import Tree from './components/Tree';
-
-function HomeScreen ({ navigation }) {
-  const [editionMode, setEditionMode] = React.useState(false);
-  const [chipTitle, setChipTitle] = React.useState('édition');
-  const [chipIcon, setChipIcon] = React.useState('tools');
-  const [ProgressBarColor, setProgressBarColor] = React.useState('');
-  const [text, setText] = React.useState('');
+const DATA = [
+  {
+    id: 1,
+    text: 'Lait',
+    children: [
+      {
+        id: 50, text: "Fraise, Orange, Citron", children: [{id: 1, text: "Mini"}, {id: 2, text: "Géant"}]
+      },
+      {
+        id: 51, text: "Pamplemousse, chocolat", children: [{id: 1, text: "Mini"}, {id: 2, text: "Géant"}]
+      },
+      {
+        id: 52, text: "Beurre fondu, cheddar, gingembre", children: [{id: 1, text: "Mini"}, {id: 2, text: "Géant"}]
+      }
+    ]
+  },
+  {
+    id: 2,
+    text: 'Thé vert',
+    children: [
+      {
+        id: 21, text: "Jasmin, huile de coude", children: [{id: 1, text: "detail"}, {id: 2, text: "detail"}]
+      },
+      {
+        id: 22, text: "Poudre d'amande, miel", children: [{id: 1, text: "Mini"}, {id: 2, text: "Géant"}]
+      },
+      {
+        id: 23, text: "Poudre de perlinpinpin, sucre", children: [{id: 1, text: "Mini"}, {id: 2, text: "Géant"}]
+      }
+    ]
+  },
+  {
+    id: 3,
+    text: 'Coffee',
+    children: [
+      {
+        id: 31, text: "Cola, sel de l'Himalaya", children: [{id: 1, text: "Mini"}, {id: 2, text: "Géant"}]
+      },
+      {
+        id: 32, text: "Butternut, radis noir, vodka", children: [{id: 1, text: "Mini"}, {id: 2, text: "Géant"}]
+      },
+      {
+        id: 33, text: "Glace au caramel, chiendent", children: [{id: 1, text: "Mini"}, {id: 2, text: "Géant"}]
+      }
+    ]
+  },
   
+]
+
+export default function App() {
+  const [data, setData] = useState(DATA)
+  const [isRender, setisRender] = useState(false)
+  const [isModalVisible, setisModalVisible] = useState(false)
+  const [inputText, setinputText] = useState()
+  const [editItem, seteditItem] = useState()
+
+  const onPressItem = (item) => {
+    setisModalVisible(true)
+    setinputText(item.text)
+    seteditItem(item.id)
+  }
+
+  const renderSubItem = (child) => {
+    let detailsItems = []
+    if (child.children) {
+      detailsItems = child.children.map(details => {
+        console.log(details.text)
+        return <Text style={styles.detailsText}>{details.text}</Text>
+      })
+    }
+    return (
+      <View>
+      <Text style={styles.subText}>{child.text}</Text>
+      {detailsItems}
+      </View>
+    )
+  }
+
+  const renderItem = ({item, index}) => {
+    let subitems = []
+    if (item.children) {
+      subitems = item.children.map(child => {
+        return renderSubItem(child)
+      })
+    }
+    return (
+      <TouchableOpacity
+        style={styles.item}
+        onPress={()=> onPressItem(item)}
+      >
+        <Text style={styles.text}>{item.text}</Text>
+        {subitems}
+        
+      </TouchableOpacity>
+      )
+  }
+
+  
+  const handleEditItem = (editItem) => {
+    const newData = data.map(item => {
+      if (item.id == editItem) {
+        item.text = inputText
+        return item
+      }
+      return item
+    })
+    setData(newData)
+    setisRender(!isRender)
+  }
+
+  const onPressSaveEdit = () => {
+    handleEditItem(editItem)
+    setisModalVisible(false)
+  }
 
   return (
-    <SafeAreaView style={{ justifyContent: "center", alignContent: "center" }}>
-      <ScrollView style={styles.scrollView}>
-        <View>
-          <Chip
-            style={{
-              backgroundColor: "#f2f2f2",
-              paddingTop: 40,
-              paddingBottom: 10,
-              justifyContent: "center",
-              alignContent: "center",
-            }}
-            mode="flat"
-            icon={chipIcon}
-            onPress={() => {
-              setEditionMode(!editionMode);
-              setChipTitle(chipTitle == "édition" ? "visiteur" : "édition");
-              setChipIcon(
-                chipIcon == "tools" ? "account-circle-outline" : "tools"
-              );
-              setProgressBarColor(chipTitle == "édition" ? Colors.red800 : "");
-            }}
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+        extraData={isRender} 
+      />
+      <Modal
+        animationType='fade'
+        visible={isModalVisible}
+        onRequestClose={()=> setisModalVisible(false)}
+      >
+        <View style={styles.modalView}>
+          <Text style={styles.text}>Change text:</Text>
+          <TextInput 
+            style={styles.textInput} 
+            onChangeText={(text)=> setinputText(text)}
+            defaultValue={inputText}
+            editable={true}
+            multiline={false}
+            maxLength={200}
+          />
+          <TouchableOpacity
+            onPress={() => onPressSaveEdit()}
+            style={styles.touchableSave}
           >
-            Passer en mode {chipTitle}
-          </Chip>
+            <Text style={styles.text}>Save</Text>
+
+          </TouchableOpacity> 
+
+
         </View>
-        <View
-          style={{
-            position: "absolute",
-            top: 0,
-            flex: 1,
-            alignSelf: "stretch",
-            right: 0,
-            left: 0,
-          }}
-        >
-          <ProgressBar progress={0.333} color={ProgressBarColor} />
-        </View>
-        <List.Section title="Personnalisez votre Bubble Tea">
-          <Tree></Tree>
-        </List.Section>
-      </ScrollView>
+
+      </Modal>
     </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  flavor: {
-    paddingLeft: 100,
-  },
-  items: {
-    paddingLeft: 200,
-  },
-}); 
-
-const Stack = createStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Accueil" component={HomeScreen} />
-        <Stack.Screen name="Commande" component={OrderScreen} />
-        <Stack.Screen name="Confirmation" component={ConfirmationScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  item: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey',
+    alignItems: 'flex-start'
+  },
+  subText: {
+    fontSize: 20,
+    marginLeft: 40,
+    marginVertical: 5
+  },
+  detailsText: {
+    fontSize: 15,
+    marginLeft: 60
+  },
+  text: {
+    marginVertical: 10,
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginLeft: 10
+  },
+  textInput: {
+    width: '90%',
+    height: 70,
+    borderColor: 'grey',
+    borderWidth: 1,
+    fontSize: 25
+  },
+  modalView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  touchableSave: {
+    backgroundColor: 'yellow',
+    paddingHorizontal: 100,
+    alignItems: 'center',
+    marginTop: 20
+  }
+});
